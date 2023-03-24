3e6f1b95de218d88e50904b6fc73915a0e23fe6d
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-03-24 13:54:47 +0530

    database connected to the UI

--- todolistcli.py
+++ todolistcli.py
@@ -2,6 +2,7 @@ import typer #here it is used to build this cli tool and rich gives us colorfull
 from rich.console import Console #typer alos helps for the commanf line interactions
 from rich.table import Table
 from database import get_all_todos, delete_todo, insert_todo, complete_todo, update_todo
+from model import Todo
 
 console = Console () # console object 
 
@@ -10,21 +11,36 @@ app = typer.Typer() # app object from typer library
 @app.command(short_help="adds an item") # app.command helps to decorate and short_help parameter provides a brief description of what the command does
 def add (task: str, category: str):
     typer.echo(f"adding{task}, {category}")
+    todo = Todo(task, category)
+    insert_todo(todo)
+
     show()
 
 @app.command()
 def delete (position: int):
     typer.echo(f"deleting {position}")
+    #herer indices in ui starts at 1, but in the database at 0
+    delete_todo(position-1)
+
     show()
 
 @app.command()
 def update(position: int, task: str = None, category: str = None):
     typer.echo(f"updating {position}")
+    update_todo(position-1, task, category)
+
+    show()
+
+@app.command()
+def complete(position: int):
+    typer.echo(f"complete{position}")
+    complete_todo(position-1)
+
     show()
 
 @app.command()
 def show():
-    tasks = [("todo1", "study"), ("todo2", "play")]
+    tasks = get_all_todos()
     console.print("[bold magenta]Todos[/bold magenta]!", "📝")
 
     table = Table(show_header=True, header_style="bold blue")
@@ -41,9 +57,9 @@ def show():
 
 
     for index, task in enumerate(tasks, start=1):
-        c = get_category_color(tasks[1])
-        is_done_str = '✅' if True == 2 else '❌'
-        table.add_row(str(index), task[0], f'[{c}]{task[1]}[/{c}]', is_done_str)
+        c = get_category_color(task.category)
+        is_done_str = '✅' if task.status == 2 else '❌'
+        table.add_row(str(index), task.task, f'[{c}]{task.category}[/{c}]', is_done_str)
     console.print(table)
 
 
