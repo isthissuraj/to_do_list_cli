8ec0bf4bbf4feadff3f137065635812c43d1db9c
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-03-24 15:05:57 +0530

    final commit

--- database.py
+++ database.py
@@ -3,32 +3,33 @@ from typing import List
 import datetime
 from model import Todo
 
-connection = sqlite3.connect("todos.db")
-c = connection.cursor()
+conn = sqlite3.connect('todos.db')
+c = conn.cursor()
+
 
 def create_table():
-    c.execute("""CREATE TABLE IF NOT EXISTS todos(
-    task text,
-    category text,
-    date_added text,
-    date_completed text,
-    status integer,
-    position integer
-    )""")
+    c.execute("""CREATE TABLE IF NOT EXISTS todos (
+            task text,
+            category text,
+            date_added text,
+            date_completed text,
+            status integer,
+            position integer
+            )""")
+
 
 create_table()
 
-def insert_todo(todo: Todo):
-    c.execute()
 
 def insert_todo(todo: Todo):
-    c.execute("select count(*) From todos")
+    c.execute('select count(*) FROM todos')
     count = c.fetchone()[0]
     todo.position = count if count else 0
-    with connection:
-        c.execute("insert INTO todos VALUES (:task, :category, :date_added, :date_completed, :status, :position)" ,
-                  {'task' : todo.task, 'category' : todo.category, 'date_added':todo.date_added, 'date_completed': todo.date_completed, 'status':todo.status,'position': todo.position})
-        
+    with conn:
+        c.execute('INSERT INTO todos VALUES (:task, :category, :date_added, :date_completed, :status, :position)',
+        {'task': todo.task, 'category': todo.category, 'date_added': todo.date_added,
+         'date_completed': todo.date_completed, 'status': todo.status, 'position': todo.position })
+
 
 def get_all_todos() -> List[Todo]:
     c.execute('select * from todos')
@@ -37,34 +38,39 @@ def get_all_todos() -> List[Todo]:
     for result in results:
         todos.append(Todo(*result))
     return todos
- 
+
+
 def delete_todo(position):
-    c.execute("select count(*) from todos")
+    c.execute('select count(*) from todos')
     count = c.fetchone()[0]
 
-    with connection:
-        c.execute("DELETE from todos WHERE position = :position", {"position":position})
-        for pos in range (position + 1, count):
+    with conn:
+        c.execute("DELETE from todos WHERE position=:position", {"position": position})
+        for pos in range(position+1, count):
             change_position(pos, pos-1, False)
 
-def change_position(old_position: int, new_position: int, commit = True):
-    c.execute("UPDATE todos SET position = :position_new WHERE position = :position_old",
-              {"position_old": old_position, "position_new": new_position})
-    if commit :
-        connection.commit()
 
-def update_todo(position : int, task: str, category: str):
-    with connection:
+def change_position(old_position: int, new_position: int, commit=True):
+    c.execute('UPDATE todos SET position = :position_new WHERE position = :position_old',
+                {'position_old': old_position, 'position_new': new_position})
+    if commit:
+        conn.commit()
+
+
+def update_todo(position: int, task: str, category: str):
+    with conn:
         if task is not None and category is not None:
-            c.execute('UPDATE todos SET  task = :task, category = :category WHERE position = :position',
-                      {'position': position, 'task':task, 'category':category}) 
+            c.execute('UPDATE todos SET task = :task, category = :category WHERE position = :position',
+                      {'position': position, 'task': task, 'category': category})
         elif task is not None:
             c.execute('UPDATE todos SET task = :task WHERE position = :position',
-                      {"position":position, "task":task})
+                      {'position': position, 'task': task})
         elif category is not None:
-            c.execute('UPDATE todos SET category =:category WHERE position = :position',
-                      {"position": position, "category": category})
+            c.execute('UPDATE todos SET category = :category WHERE position = :position',
+                      {'position': position, 'category': category})
+
+
 def complete_todo(position: int):
-    with connection:
-        c.execute("UPDATE todos SET status = 2, date_completed =:date_completed WHERE position = :position",
-                  {"position": position, 'date_completed': datetime.datetime.now().isoformat()})
+    with conn:
+        c.execute('UPDATE todos SET status = 2, date_completed = :date_completed WHERE position = :position',
+                  {'position': position, 'date_completed': datetime.datetime.now().isoformat()})
--- todolistcli.py
+++ todolistcli.py
@@ -1,47 +1,51 @@
-import typer #here it is used to build this cli tool and rich gives us colorfull texts
-from rich.console import Console #typer alos helps for the commanf line interactions
+import pyfiglet
+from termcolor import colored
+import typer
+from rich.console import Console
 from rich.table import Table
-from database import get_all_todos, delete_todo, insert_todo, complete_todo, update_todo
 from model import Todo
+from database import get_all_todos, delete_todo, insert_todo, complete_todo, update_todo
 
-console = Console () # console object 
+text = pyfiglet.figlet_format("TODO LIST")
+colored_art = colored(text, color = "green")
+print(colored_art)
+print("****************************************************************************************")
 
-app = typer.Typer() # app object from typer library
+console = Console()
 
-@app.command(short_help="adds an item") # app.command helps to decorate and short_help parameter provides a brief description of what the command does
-def add (task: str, category: str):
-    typer.echo(f"adding{task}, {category}")
+app = typer.Typer()
+
+
+@app.command(short_help='adds an item')
+def add(task: str, category: str):
+    typer.echo(f"adding {task}, {category}")
     todo = Todo(task, category)
     insert_todo(todo)
-
     show()
 
 @app.command()
-def delete (position: int):
+def delete(position: int):
     typer.echo(f"deleting {position}")
-    #herer indices in ui starts at 1, but in the database at 0
+    # indices in UI begin at 1, but in database at 0
     delete_todo(position-1)
-
     show()
 
 @app.command()
 def update(position: int, task: str = None, category: str = None):
     typer.echo(f"updating {position}")
     update_todo(position-1, task, category)
-
     show()
 
 @app.command()
 def complete(position: int):
-    typer.echo(f"complete{position}")
+    typer.echo(f"complete {position}")
     complete_todo(position-1)
-
     show()
 
 @app.command()
 def show():
     tasks = get_all_todos()
-    console.print("[bold magenta]Todos[/bold magenta]!", "📝")
+    console.print("[bold magenta]Todos[/bold magenta]!", "💻")
 
     table = Table(show_header=True, header_style="bold blue")
     table.add_column("#", style="dim", width=6)
@@ -50,16 +54,15 @@ def show():
     table.add_column("Done", min_width=12, justify="right")
 
     def get_category_color(category):
-        COLORS = {"Learn": "cyan", "Youtube": "red", "sports": "cyan", "study":"green"}
+        COLORS = {'Learn': 'cyan', 'YouTube': 'red', 'Sports': 'cyan', 'Study': 'green'}
         if category in COLORS:
             return COLORS[category]
-        return "white"
+        return 'white'
 
-
-    for index, task in enumerate(tasks, start=1):
+    for idx, task in enumerate(tasks, start=1):
         c = get_category_color(task.category)
         is_done_str = '✅' if task.status == 2 else '❌'
-        table.add_row(str(index), task.task, f'[{c}]{task.category}[/{c}]', is_done_str)
+        table.add_row(str(idx), task.task, f'[{c}]{task.category}[/{c}]', is_done_str)
     console.print(table)
 
 
